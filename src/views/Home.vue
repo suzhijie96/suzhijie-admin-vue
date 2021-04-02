9ea81ab9ea484259ed9d98b0aea30814ca017f03
suzhijie <suzhijie@bigtreefinance.com>
2021-04-02 17:40:58 +0800

    feat: 全局css配置

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Login />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import Login from "@/views/account/login.vue";
 
 export default defineComponent({
   name: "Home",
   components: {
-    HelloWorld,
+    Login,
   },
 });
 </script>
---
+++ src/views/account/login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="login">
+    <a-button>按钮</a-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+};
+</script>
+
+<style></style>
